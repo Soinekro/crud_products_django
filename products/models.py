e447744c32f164375947988402c8c048d72711e7
Soinekro <echumaceroj@ucvvirtual.edu.pe>
2024-10-02 18:36:27 -0500

    crud en admin para productos

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.forms import ModelForm
 
 # Create your models here.
+class Product(models.Model):
+    # nombre unico
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Product
 
 def index(request):
-    return HttpResponse('Hello, World! products index page')
+    lastes_products_list = Product.objects.order_by("-created_at")[:5]
+    output = ", ".join([q.name for q in lastes_products_list])
+    return HttpResponse(output)
